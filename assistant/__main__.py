e6c10a2f1ddb7328f4cd84f241718ff18c54cf1e
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-06-30 18:45:21 +0600

    Update

--- assistant/__main__.py
+++ assistant/__main__.py
@@ -3,41 +3,46 @@ import speech_recognition as sr
 from . import logger, recognizer
 from .core.jarvis import JARVIS_DATA, speakJarvis
 
-# Starting Message from JARVIS
-speakJarvis(JARVIS_DATA.GREET)
+def main():
+    # Starting Message from JARVIS
+    speakJarvis(JARVIS_DATA.GREET)
 
-while True:
-    # Use microphone as source
-    with sr.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source) # Reduce background noise
-        audio = recognizer.listen(source)
+    while True:
+        # Use microphone as source
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source) # Reduce background noise
+            audio = recognizer.listen(source)
 
-        try:
-            text = recognizer.recognize_google(audio).lower()
-            logger.info(f"ME: {text}")
+            try:
+                text = recognizer.recognize_google(audio).lower()
+                logger.info(f"ME: {text}")
 
-            # added `jar` cuz it sometime can't detect word `jarvis`
-            if "jar" not in text and "jarvis" not in text:
-                continue
+                # added `jar` cuz it sometime can't detect word `jarvis`
+                if "jar" not in text and "jarvis" not in text:
+                    continue
 
-            # Logics
-            if "time" in text:
-                now = datetime.now()
-                current_time = now.strftime("%I:%M %p") # Format like "03:45 PM"
-                speakJarvis(f"The current time is {current_time}")
-            
-            elif "thanks" in text or "thank you" in text:
-                speakJarvis(JARVIS_DATA.THANKS)
-            
-            # End the call
-            elif "bye" in text:
-                speakJarvis(JARVIS_DATA.BYE)
-                break
-            
-            else:
-                speakJarvis(JARVIS_DATA.GREET)
-            
-        except sr.UnknownValueError:
-            pass
-        except sr.RequestError as e:
-            logger.error(e)
+                # Logics
+                if "time" in text:
+                    now = datetime.now()
+                    current_time = now.strftime("%I:%M %p") # Format like "03:45 PM"
+                    speakJarvis(f"The current time is {current_time}")
+                
+                elif "thanks" in text or "thank you" in text:
+                    speakJarvis(JARVIS_DATA.THANKS)
+                
+                # End the call
+                elif "bye" in text:
+                    speakJarvis(JARVIS_DATA.BYE)
+                    break
+                
+                else:
+                    speakJarvis(JARVIS_DATA.RESPONSE)
+                
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError as e:
+                logger.error(e)
+
+
+if __name__ == "__main__":
+    main()
--- assistant/core/jarvis.py
+++ assistant/core/jarvis.py
@@ -1,9 +1,11 @@
 import random
+from datetime import datetime
 from assistant import logger, tts_engine
 
 class JARVIS_DATA:
-    GREET = ["Hi sir!", "Greeting sir!", "I'm here sir!", "At your service sir!"]
+    GREET = []
     BYE = ["See you sir!", "Have a good day sir!", "Bye sir!"]
+    RESPONSE = ["Hi sir!", "I'm here sir!", "At your service sir!"]
     THANKS = ["Always at your service sir!", "Ohh, don't thank me sir! It's my JOB sir!", "Appreciate it sir!"]
 
 
@@ -12,7 +14,21 @@ def speakJarvis(text):
     :param text: Cloud be any `text` or `JARVIS_DATA` e.g. JARVIS_DATA.GREET
     """
 
-    if text in [JARVIS_DATA.GREET, JARVIS_DATA.BYE, JARVIS_DATA.THANKS]:
+    if text in [JARVIS_DATA.GREET, JARVIS_DATA.BYE, JARVIS_DATA.RESPONSE, JARVIS_DATA.THANKS]:
+        if text in [JARVIS_DATA.GREET]:
+            # day time calculator
+            hour = datetime.now().hour
+            if 6 <= hour <= 11:
+                daytime = "Morning"
+            elif 12 <= hour <= 15:
+                daytime = "Noon"
+            elif 16 <= hour <= 18:
+                daytime = "Evening"
+            elif 19 <= hour <= 5:
+                daytime = "Night"
+            
+            text.append(f"Good {daytime} sir!")
+        
         text = random.choice(text)
     
     try:
